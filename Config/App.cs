a716b8f7b3fda093fc7e4407fe3564c8122dc9d0
Hans Chan <1148706823@qq.com>
2025-06-18 11:58:17 +0800

    snap: add some

---
+++ Config/App.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotEnv.Core;
+
+namespace Agent.Config
+{
+    public class App
+    {
+        [EnvKey("AWS_ACCESS_KEY_ID")]
+        public string AwsAccessKeyId { get; set; }
+
+        [EnvKey("AWS_SECRET_ACCESS_KEY")]
+        public string AwsSecretAccessKey { get; set; }
+
+        [EnvKey("AWS_DEFAULT_REGION")]
+        public string AwsDefaultRegion { get; set; }
+
+        [EnvKey("FEISHU_SECRET_ID")]
+        public string FershuSecretId { get; set; }
+
+        [EnvKey("FEISHU_SECRET_KEY")]
+        public string FeishuSecretKey { get; set; }
+
+    }
+}
