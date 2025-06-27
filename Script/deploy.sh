cbc912c0396a79a16499500be1b97965a889df49
Hans Chan <1148706823@qq.com>
2025-06-27 20:50:28 +0800

    snap: deploy.sh

--- Script/deploy.sh
+++ Script/deploy.sh
@@ -14,10 +14,11 @@ if [ $# -eq 0 ]; then
 fi
 VERSION=$1
 
-
+echo "[INFO] 下载 agent-cli-v1.0.0-alpha.1-linux-x64 "
 sudo mkdir -p /opt/agent-cli
 sudo wget  https://github.com/harmful-chan/agent/releases/download/v1.0.0-alpha.1/agent-cli-v1.0.0-alpha.1-linux-x64 -o /opt/agent-cli/agent-cli-v1.0.0-alpha.1-linux-x64
 sudo ln -s /opt/agent-cli/agent-cli-v1.0.0-alpha.1-linux-x64 /opt/agent-cli/agent-cli
+echo "[INFO] 创建 /etc/systemd/system/agent-cli.service"
 # 写入服务进程
 sudo cat >/etc/systemd/system/agent-cli.service <<EOF
 [Unit]
