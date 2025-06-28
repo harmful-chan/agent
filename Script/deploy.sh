7beb2346dc333e53d5eb57823da54bd46317eb87
Hans Chan <1148706823@qq.com>
2025-06-28 17:15:02 +0800

    build: 修改 deploy.sh

--- Script/deploy.sh
+++ Script/deploy.sh
@@ -12,24 +12,23 @@ VERSION=$1
 RELEASES=agent-cli-${VERSION}-linux-x64
 WORKDIR=/opt/flori/agent
 
-sudo mkdir -p $WORKDIR
-pushd $WORKDIR
 
-sudo systemctl stop flori-agent
-# 下载可执行文件，并创建软连接
-if [ ! -f "$RELEASES" ]; then
-	echo "[INFO] 下载 $RELEASES "
-	sudo wget  "https://git.floribird.com/https://github.com/harmful-chan/agent/releases/download/${VERSION}/${RELEASES}"
-	sudo chmod a+x $RELEASES
-	sudo chown root:root $RELEASES
-	sudo ln -sf $PWD/$RELEASES $PWD/flori-agent
-fi
+function download(){
+	# 下载可执行文件，并创建软连接
+	if [ ! -f "$RELEASES" ]; then
+		echo "[INFO] 下载 $RELEASES "
+		sudo wget  "https://git.floribird.com/https://github.com/harmful-chan/agent/releases/download/${VERSION}/${RELEASES}"
+		sudo chmod a+x $RELEASES
+		sudo chown root:root $RELEASES
+	fi
+}
 
 
-if [ ! -f /etc/systemd/system/flori-agent.service ]; then
-	# 写入服务进程
-	echo "[INFO] 创建 /etc/systemd/system/flori-agent.service"
-	sudo tee flori-agent.service > /dev/null<<EOF
+function create_service(){
+	if [ ! -f /etc/systemd/system/flori-agent.service ]; then
+		# 写入服务进程
+		echo "[INFO] 创建 /etc/systemd/system/flori-agent.service"
+		sudo tee flori-agent.service > /dev/null<<EOF
 [Unit]
 Description=Agent Cli Service for .NET 8 Console Application
 After=network.target
@@ -53,13 +52,19 @@ TimeoutStopSec=30
 [Install]
 WantedBy=multi-user.target
 EOF
-	sudo mv flori-agent.service /etc/systemd/system/flori-agent.service
-	sudo chown root:root /etc/systemd/system/flori-agent.service
-	sudo systemctl daemon-reload
-	sudo systemctl enable flori-agent
-fi
+		sudo mv flori-agent.service /etc/systemd/system/flori-agent.service
+		sudo chown root:root /etc/systemd/system/flori-agent.service
+		sudo systemctl daemon-reload
+		sudo systemctl enable flori-agent
+	fi
+}
 
-
+sudo mkdir -p $WORKDIR
+pushd $WORKDIR
+sudo systemctl stop flori-agent
+download
+sudo ln -sf $PWD/$RELEASES $PWD/flori-agent
+create_service
 sudo systemctl start flori-agent
 
 popd
